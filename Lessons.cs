7198a199e0f80b1cd21f79f8821e3ed8101f8663
gwynbleidd10 <gwynbleidd10@gmail.com>
2018-03-19 18:06:08 +0900

    Lessons v1, without Employee

---
+++ Lessons.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lessons
+{
+    public class Lessons
+    {
+        //private Employee lect;
+        private string _name;
+        private DateTime _date;
+
+        //Setters and Getters
+
+        public string name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public DateTime date
+        {
+            get
+            {
+                return this._date;
+            }
+            set
+            {
+                this._date = value;
+            }
+        }
+
+        /*public Object lecturer
+        {
+            get
+            {
+                return this.lect;
+            }
+            set
+            {
+                this.lect = value;
+            }
+        }
+        */
+    }
+}
