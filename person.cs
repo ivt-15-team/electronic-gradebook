e315210820639ac92cd2ec086bf7ec96724c40b0
Carl Ivanov <bdgtw502@mail.ru>
2018-03-19 18:08:45 +0900

    Класс персоны

---
+++ person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace person
+{
+    public class Person
+    {
+        string FirstName
+        {
+            get
+            {
+                return this.FirstName;
+            }
+            set
+            {
+                this.FirstName = value;
+            }
+        }
+        string SecondName
+        {
+            get
+            {
+                return this.SecondName;
+            }
+            set
+            {
+                this.SecondName = value;
+            }
+        }
+        string MiddleName
+        {
+            get
+            {
+                return this.MiddleName;
+            }
+            set
+            {
+                this.MiddleName = value;
+            }
+        }
+        DateTime BirthDate
+        {
+            get
+            {
+                return this.BirthDate;
+            }
+            set
+            {
+                this.BirthDate = value;
+            }
+        }
+
+        Person(string _fname, string _sname, string _mname, DateTime _bdate)
+        {
+            FirstName = _fname;
+            SecondName = _sname;
+            MiddleName = _mname;
+            BirthDate = _bdate;
+        }
+    }
+}
