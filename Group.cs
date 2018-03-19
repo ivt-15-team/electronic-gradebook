ceb0267b5faf81ca3468bb89f4351c51805a8476
vooch322 <36365299+vooch322@users.noreply.github.com>
2018-03-19 18:37:32 +0900

    with the Lesson.cs Employee.cs Lesson.cs

--- Group.cs
+++ Group.cs
@@ -3,7 +3,8 @@ public class Group
         private string group_name;
         private int year;
         private EDU_plan edu_plan;
-        
+        private Employee employee;
+        private Lesson lesson;
         public string Group_name
         {
             get
@@ -29,11 +30,13 @@ public class Group
 
 
 
-        public Group(string groupname, int year_enter,EDU_plan Edu_plan)
+        public Group(string groupname, int year_enter,EDU_plan Edu_plan,Employee employe1,Lesson less)
         {
             group_name = groupname;
             year = year_enter;
             edu_plan = Edu_plan;
+            employee = employe1;
+            lesson = less;
         }
         
        
