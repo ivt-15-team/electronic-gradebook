04a7f910f906054c9bae835774667b2f23c0b5c7
BJladika <BJladika@gmail.com>
2018-03-19 18:07:22 +0900

    Add files via upload

---
+++ Edu_Plan_Content.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edu_Plan_Content
+{
+    public class Edu_Plan_Content
+    {
+        ///private EduPlan eduPlan;
+        //private Subject subject;
+        //private Employee employee;
+        //private ControlType controlType;
+        /////////////
+        private string _subject_id;
+        private int _total_hours;
+        public string subject_id
+        {
+            get
+            {
+                return this._subject_id;
+            }
+            set
+            {
+                this._subject_id = value;
+            }
+        }
+        public int total_hours
+        {
+            get
+            {
+                return this._total_hours;
+            }
+            set
+            {
+                this._total_hours = value;
+            }
+        }
+    }
+}
