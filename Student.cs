5b1afe21d4f6e378f528cfd6e50840bf7d45ee85
cadfoot <indiegeargames@gmail.com>
2018-03-19 17:41:44 +0900

    +Student model

---
+++ Student.cs
@@ -0,0 +1,8 @@
+﻿//waiting for parent classes to be implemented
+//public class Student : Person
+public class Student
+{
+    public string GroupName { get; private set; }
+    public int Id { get; private set; }
+    public string Status { get; private set; }
+}
