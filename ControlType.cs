e2d37154d9087b7497d3d4cfbf993cfeb1a904c9
123Ace <31559470+123Ace@users.noreply.github.com>
2018-03-19 18:24:23 +0900

    ControlType

---
+++ ControlType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlType
+{
+    class ControlType
+    {
+        public class ControlType
+        {
+            private string _name
+            {
+                get
+                {
+                    return this._name;
+                }
+                set
+                {
+                    this._name = value;
+                }
+            }
+
+
+        }
+    }
+}
