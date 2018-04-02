e27bdfa22865fa625bde58f885e09bdb65261cb7
cadfoot <indiegear@gmail.com>
2018-04-02 17:23:43 +0900

    ef initial

---
+++ Ebook/EbookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebook
+{
+    class EbookContext : DbContext
+    {
+        public EbookContext() : base("Data Source =.\\ebook.db")
+        {
+
+        }
+    
+        public DbSet<User> Users { get; set; }
+    }
+}
