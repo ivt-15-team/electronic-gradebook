6faeb5847185a8720ed005f33acd88cf7d2c4a46
jekkery <jekkery@gmail.com>
2018-04-09 19:06:55 +0900

    repo added to employee

--- Ebook/employee.cs
+++ Ebook/employee.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Ebook
 {
@@ -43,4 +46,61 @@ namespace Ebook
 
 
     }
+
+    public class EmployeeRepository : IRepository<Employee>, IDisposable
+    {
+        private EbookContext context;
+
+        public EmployeeRepository(EbookContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Employee> GetEntities()
+        {
+            return context.Employees.ToList();
+        }
+        public Employee GetEntityById(int id)
+        {
+            return context.Employees.Find(id);
+        }
+        public void InsertEntity(Employee employee)
+        {
+            context.Employees.Add(employee);
+        }
+        public void DeleteEntity(int employeeID)
+        {
+            Employee employee = context.Employees.Find(employeeID);
+            context.Employees.Remove(employee);
+        }
+        public void UpdateEntity(Employee employee)
+        {
+            context.Entry(employee).State = EntityState.Modified;
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
 }
+
