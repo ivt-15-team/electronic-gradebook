7b6d9f36f9e7d36e0e129350483eebea6d785107
cadfoot <indiegeargames@gmail.com>
2018-04-09 18:20:49 +0900

    +Student Reps

---
+++ Ebook/Repositories/StudentRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Ebook.Repositories
+{
+    class StudentRepository : IRepository<Student>
+    {
+        private EbookContext context;
+
+        public StudentRepository(EbookContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteEntity(int entityID)
+        {
+            Student student = context.Students.Find(entityID);
+            context.Students.Remove(student);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public IEnumerable<Student> GetEntities()
+        {
+            return context.Students.ToList();
+        }
+
+        public Student GetEntityById(int entityID)
+        {
+            return context.Students.Find(entityID);
+        }
+
+        public void InsertEntity(Student entity)
+        {
+            context.Students.Add(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateEntity(Student entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
