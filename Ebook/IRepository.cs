64912b6e3044e17baff2c5bc9f3d09ca8f8498db
cadfoot <indiegeargames@gmail.com>
2018-04-09 17:59:38 +0900

    +irepository

---
+++ Ebook/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ebook
+{
+    interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetEntities();
+        T GetEntityById(int entityID);
+        void InsertEntity(T entity);
+        void DeleteEntity(int entityID);
+        void UpdateEntity(T entity);
+        void Save();
+    }
+}
