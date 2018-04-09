ff28a046bbf4695a3b99a65f0c1e771630f0be33
cadfoot <indiegeargames@gmail.com>
2018-04-09 18:50:46 +0900

    +EduplanRepository

---
+++ Ebook/Repositories/EduplanRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Ebook.Repositories
+{
+    class EduplanRepository : IRepository<EDU_plan>
+    {
+        private EbookContext context;
+
+        public EduplanRepository(EbookContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteEntity(int entityID)
+        {
+            var eduplan = context.EduPlans.Find(entityID);
+            context.EduPlans.Remove(eduplan);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public IEnumerable<EDU_plan> GetEntities()
+        {
+            return context.EduPlans.ToList();
+        }
+
+        public EDU_plan GetEntityById(int entityID)
+        {
+            return context.EduPlans.Find(entityID);
+        }
+
+        public void InsertEntity(EDU_plan entity)
+        {
+            context.EduPlans.Add(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateEntity(EDU_plan entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
