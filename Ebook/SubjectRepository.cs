ea23eb88323ae378fd96d54bfd61896c29e0afa4
user <mail@mail.com>
2018-04-16 16:20:00 +0900

    Added SubjectRepository.cs

---
+++ Ebook/SubjectRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Ebook
+{
+	class SubjectRepository:IRepository<Subject>,IDisposable
+	{
+        EbookContext context;
+        public SubjectRepository(EbookContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Subject> GetEntities()
+        {
+            return context.Subject.ToList();
+        }
+        public Subject GetEntityById(int entityID)
+        {
+            return context.Subject.Find(entityID);
+        }
+        public void InsertEntity(Subject entity)
+        {
+            context.Subject.Add(entity);
+
+        }
+        public void DeleteEntity(int entityID)
+        {
+            Subject subject = context.Subject.Find(entityID);
+            context.Subject.Remove(subject);
+        }
+        public void UpdateEntity(Subject entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this.disposed)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+        }
+        this.disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
+}
